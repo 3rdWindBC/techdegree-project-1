c94350260015a1f3510cc1308985c917abc4c7a9
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-09-20 06:37:49 -0700

    Lists
    Added list to keep track of number of attempts. Added list for high score but this is not completed yet.

---
+++ guessing_game.py
@@ -0,0 +1,45 @@
+
+#Python Web Development Techdegree
+#Project 1 - Number Guessing Game
+#--------------------------------
+
+
+import random
+
+print("Welcome to the NUMBER GUESSING GAME!!!")
+
+answer_attempts = []
+highscore = []
+
+def start_game():
+
+    for solution in range(10):
+        solution = random.randint(1,10)
+    
+    answer = int(input("Guess a number between 1-10: "))
+        
+        if answer < solution:
+            answer_attempts.append(answer)
+            print("It's higher")
+            answer = int(input("Guess a number between 1-10: ")) 
+
+        if answer > solution:
+            answer_attempts.append(answer)
+            print("It's lower!")
+            answer = int(input("Guess a number between 1-10: "))
+                     
+    answer_attempts.append(answer)
+    print("Got it")
+            
+    play_again = input("Would you like to play again? (y)es or (n)o: ")
+    if play_again.lower() == "y":
+        start_game()
+    else:
+        print("It took you {} attempts.".format(len(answer_attempts)))
+        print("Goodbye")
+
+
+
+
+    
+start_game()
