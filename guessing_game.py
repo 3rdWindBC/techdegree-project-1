6f16a4e2e11dc988fbd298f5989fe4dfca69a8a4
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-09-22 10:35:13 -0700

    corrections of errors
    Modified answers to correct for exceptions that occurred with responses outside of 1-10. Added some blank lines for a cleaner look. Added more flexibility in what can be entered to continue playing.

--- guessing_game.py
+++ guessing_game.py
@@ -6,7 +6,7 @@
 
 import random
 
-print("Welcome to the NUMBER GUESSING GAME!!!\n")
+print("\nWelcome to the NUMBER GUESSING GAME!!!\n")
 
 
 highscore = []
@@ -15,37 +15,41 @@ def start_game():
     answer_attempts = []
 
     solution = random.randint(1,10)
-
+    
     try:
         answer = int(input("--> Guess a number between 1-10: "))
-    
         while answer != solution:
-        
-            if answer < solution:
+            if answer < solution and answer > 0:
                 answer_attempts.append(answer)
-                print("You're guess is too low")
+                print("It's higher")
                 answer = int(input("--> Guess a number between 1-10: ")) 
 
-            if answer > solution:
+            if answer > solution and answer < 11:
                 answer_attempts.append(answer)
-                print("You're guess is too high")
+                print("It's lower")
                 answer = int(input("--> Guess a number between 1-10: "))
+
+            if answer < 1 or answer > 10:
+                answer_attempts.append(answer)
+                print("That number is outside of the range, try again.\n")
+                answer = int(input("--> Guess a number between 1-10: "))
+           
     except ValueError:
-        print("Seriously?? Not a valid response... Start over!!\n")
+        print("Seriously?? Not a valid response...START OVER\n")
         start_game()
                      
     answer_attempts.append(answer)
-    print("YOU GOT IT!!!")
+    print("\nYOU GOT IT!!!")
     print("It took you {} attempts.\n".format(len(answer_attempts)))
     highscore.append(len(answer_attempts))
 
     play_again = input("Would you like to play again? (y)es or (n)o: ")
-    if play_again.lower() == "y":
+    if play_again.lower() == "y" or play_again.lower() == "yes":
         print("\nHighscore = {}\n".format(min(highscore)))
         start_game()
     else:
         print("Final Highscore = {}\n".format(min(highscore)))
         print("Goodbye")
-
+    
     
 start_game()
