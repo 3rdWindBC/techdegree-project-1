2dd2b2287097abff9888514fbb79f6c45f7c4b3e
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-09-22 11:30:39 -0700

    Trying to fix errors occuring when floats are entered
    Moved the answer == solution section into the while loop.

--- guessing_game.py
+++ guessing_game.py
@@ -21,35 +21,34 @@ def start_game():
         while answer != solution:
             if answer < solution and answer > 0:
                 answer_attempts.append(answer)
-                print("It's higher")
+                print("It's higher\n")
                 answer = int(input("--> Guess a number between 1-10: ")) 
 
             if answer > solution and answer < 11:
                 answer_attempts.append(answer)
-                print("It's lower")
+                print("It's lower\n")
                 answer = int(input("--> Guess a number between 1-10: "))
 
             if answer < 1 or answer > 10:
                 answer_attempts.append(answer)
                 print("That number is outside of the range, try again.\n")
                 answer = int(input("--> Guess a number between 1-10: "))
-           
+
+            if answer == solution:
+                answer_attempts.append(answer)
+                print("\nYOU GOT IT!!!")
+                print("It took you {} attempts.\n".format(len(answer_attempts)))
+                highscore.append(len(answer_attempts))
+                play_again = input("Would you like to play again? (y)es or (n)o: ")
+                if play_again.lower() == "y" or play_again.lower() == "yes":
+                    print("\nHighscore = {}\n".format(min(highscore)))
+                    start_game()
+                else:
+                    print("Final Highscore = {}\n".format(min(highscore)))
+                    print("Goodbye")
+                
     except ValueError:
         print("Seriously?? Not a valid response...START OVER\n")
         start_game()
                      
-    answer_attempts.append(answer)
-    print("\nYOU GOT IT!!!")
-    print("It took you {} attempts.\n".format(len(answer_attempts)))
-    highscore.append(len(answer_attempts))
-
-    play_again = input("Would you like to play again? (y)es or (n)o: ")
-    if play_again.lower() == "y" or play_again.lower() == "yes":
-        print("\nHighscore = {}\n".format(min(highscore)))
-        start_game()
-    else:
-        print("Final Highscore = {}\n".format(min(highscore)))
-        print("Goodbye")
-    
-    
 start_game()
